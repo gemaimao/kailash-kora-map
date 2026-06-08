62c4e6cf43ea0e14f6654431094dd57e16981a3b
gemaimao <gemaimao@163.com>
2026-06-08 13:45:16 +0800

    fix: define missing iconUrl for map billboards

--- 3d/js/app.js
+++ 3d/js/app.js
@@ -137,6 +137,7 @@ function loadPoisAndStart(terrainProvider) {
                     });
                 }
 
+                const iconUrl = `https://raw.githubusercontent.com/gemaimao/assets/main/kailashpic/${poiId}.png`;
                 viewer.entities.add({
                     id: 'poi_' + poi.id,
                     position: Cesium.Cartesian3.fromDegrees(fixedLng, fixedLat, poi.flat ? groundHeight : altitude),
